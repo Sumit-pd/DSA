944fc7cc6ba26232757398a1128f32a13541139f
sumit <pdsumit07@gmail.com>
2023-08-19 16:50:28 +0530

    update

--- arrays/majorityElement_2.cpp
+++ arrays/majorityElement_2.cpp
@@ -30,7 +30,6 @@ vector<int> majorityElement(vector<int> arr)
 }
 
 
-#include <bits/stdc++.h>
 vector<int> majorityElement(vector<int> v) {
 	int n = v.size() ;
 	unordered_map<int,int> mp ;
@@ -51,4 +50,54 @@ vector<int> majorityElement(vector<int> v) {
     // time complexity O(nlogn) 
     //space complexity O(n) for the map
 	
+}
+
+vector<int> majorityElement(vector<int> v) {
+    int element1 = INT_MIN;
+    int element2 = INT_MIN;
+    int cnt1 = 0;
+    int cnt2 = 0;
+    vector<int> ans;
+    int n = v.size();
+
+    for (int i = 0; i < n; i++) {
+        if (cnt1 == 0 && element2 != v[i]) {
+            cnt1 = 1;
+            element1 = v[i];
+        } else if (cnt2 == 0 && element1 != v[i]) {
+            cnt2 = 1;
+            element2 = v[i];
+        } else if (element1 == v[i]) {
+            cnt1++;
+        } else if (element2 == v[i]) {
+            cnt2++;
+        } else {
+            cnt1--;  // Corrected: cnt1--
+            cnt2--;  // Corrected: cnt2--
+        }
+    }  
+
+    cnt1 = 0;
+    cnt2 = 0;
+    for (int i = 0; i < n; i++) {
+        if (v[i] == element1) {
+            cnt1++;
+        } else if (v[i] == element2) {
+            cnt2++;
+        }
+    }
+
+    int mini = (int)n / 3 + 1;
+    if (cnt1 >= mini) {
+        ans.push_back(element1);
+    }
+    if (cnt2 >= mini) {
+        ans.push_back(element2);
+    }
+
+    sort(ans.begin(), ans.end()); 
+    return ans;
+
+    //time complexity : O(N)
+    //space complexity : O(1)
 }
