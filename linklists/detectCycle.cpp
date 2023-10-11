2867e92b322654749053df889a8546dbc9775fee
sumit <pdsumit07@gmail.com>
2023-10-11 21:10:30 +0530

    problem solved

--- linklists/detectCycle.cpp
+++ linklists/detectCycle.cpp
@@ -1,8 +1,21 @@
+#include <bits/stdc++.h>
+bool detectCycle(Node *head)
+{
+	unordered_set<Node*> st ;
+    while(head != NULL ){
+        if(st.find(head) != st.end() ){
+            return true ;
+        }
+        st.insert(head) ;
+        head = head -> next ;
+    }
+    return false ;
+}
 bool detectCycle(Node *head)
 {
 	Node * slow = head ;
     Node * fast = head ;
-    while(fast -> next != NULL && fast -> next -> next != NULL ){
+    while(fast != NULL && fast -> next != NULL && fast -> next -> next != NULL ){
         slow = slow -> next ;
         fast = fast -> next -> next ;
         if(slow == fast){
