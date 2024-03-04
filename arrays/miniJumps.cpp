a7aa5244b0a861c27c208ca5c071f5b8d6dbb3ea
sumit <pdsumit07@gmail.com>
2024-03-04 19:42:59 +0530

    added problem

--- arrays/miniJumps.cpp
+++ arrays/miniJumps.cpp
@@ -1,26 +1,29 @@
-#include <bits/stdc++.h> 
-int minimumJumps(vector<int> &arr, int n){
-    int maxReach = 0 ; // this will be the max reach 
-    int lastJump = 0 ; // this will be the place 
-    int jumps = 0 ;
-    if(n == 1 ){
-        return 0 ;
-    }
-    if(arr[0] == 0 ){
-        return -1 ;
-    }
-    for(int i = 0 ; i < n ; i++ ){
-        if(i > maxReach){
+class Solution{
+  public:
+    int minJumps(int arr[], int n){
+        int maxReach = 0 ; // this will be the max reach 
+        int lastJump = 0 ; // this will be the place 
+        int jumps = 0;
+        if(n == 1 ){
+            return 0 ;
+        }
+        if(arr[0] == 0 ){
             return -1 ;
         }
-        maxReach = max(maxReach , i + arr[i] );
-        if(lastJump == i ){
-            lastJump = maxReach ;
-            jumps++ ;
+        for(int i = 0 ; i < n ; i++ ){
+            if(i > maxReach){
+                return -1 ;
+            }
+            maxReach = max(maxReach , i + arr[i] );
+            if(lastJump == i ){
+                lastJump = maxReach ;
+                jumps++ ;
+                if(maxReach >= n -1 ){
+                return jumps ;
+            }
+            }
+            
         }
-        if(maxReach >= n -1 ){
-            return jumps ;
-        }
+        return jumps ;
     }
-    return jumps ;
-}
+};
--- arrays/minimizeTheMaxDifference.cpp
+++ arrays/minimizeTheMaxDifference.cpp
@@ -2,13 +2,12 @@
 int minimizeMaximum(int arr[], int n, int k)
 {
     sort(arr,arr+n) ;
-    int ans = (arr[n -1])  - (arr[0]) ;
+    int ans = arr[n -1]  - arr[0] ;
     for(int i = 1 ; i < n ; i++ ){
-        int maxi = max(arr[i-1] + k ,  arr[n-1] - k) ;
         int mini = min(arr[i] - k , arr[0] + k) ;
+        int maxi = max(arr[i-1] + k ,  arr[n-1] - k) ;
         // we are only comparing the mini and maxi with the last element because we can only add and subract once
         ans = min(ans , maxi - mini) ;
     }
-    return ans ;
-    
+    return ans ;  
 }
