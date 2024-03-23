020d2d22596f6b382af7ab3f80e45576a7ea1675
sumit <pdsumit07@gmail.com>
2024-03-23 11:10:35 +0530

    added question

--- searching and sorting/5.adjDifferKArr.cpp
+++ searching and sorting/5.adjDifferKArr.cpp
@@ -16,7 +16,7 @@ int search(int arr[], int n, int x, int k)
         if(arr[i] == x ){
             return i ;
         }
-        int steps = abs(arr[i]-x) / k ;
+        int steps = abs(arr[i]-x) / k ; // here the arr[i] is the element of the array and x is the target and k is the difference
         //moving at max k steps
         steps = max(steps,1); // there will be case when the value of k will be 0 which will be resulting in infinite loop
         i += steps;
