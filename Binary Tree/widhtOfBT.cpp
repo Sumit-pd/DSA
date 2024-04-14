d51911b1b068c49244d94210b5d5ca98ff4368b2
sumit <pdsumit07@gmail.com>
2024-04-14 21:10:11 +0530

    update code

--- Binary Tree/widhtOfBT.cpp
+++ Binary Tree/widhtOfBT.cpp
@@ -17,6 +17,7 @@
     };
 
 ************************************************************/
+//the below will fail for the leetcode problem only works for codestudio
 #include<bits/stdc++.h>
 int getMaxWidth(TreeNode<int> *root)
 {
@@ -44,3 +45,74 @@ int getMaxWidth(TreeNode<int> *root)
     return maxWidth ;
 
 }
+
+int widthOfBinaryTree(TreeNode* root) {
+    if(root == NULL){
+        return 0 ;
+    }        
+    queue< pair<TreeNode<int>*,int> > q ;
+    q.push({root,0}) ;
+    int ans = 0 ;
+    while(q.size()){
+        int size = q.size();
+        int minInd = 0 ;
+        int maxInd = 0 ;
+        for (int i = 0; i < size ; i++)
+        {
+            auto front = q.front() ;
+            q.pop() ;
+            TreeNode* node = front.first ;
+            int index = front.second ;
+            minInd = min(minInd , index) ;
+            maxInd = max(maxInd , index) ;
+            if(node -> left ){
+                q.push({node -> left ,2*(index - minInd) +1 }) ;
+            }
+            if(node -> right){
+                q.push({node -> right , 2*(index - minInd)+2 }) ;
+            }
+        }
+        ans = max(ans,maxInd - minInd + 1) ;
+    }
+    return ans ; 
+}
+
+
+class Solution {
+public:
+    int widthOfBinaryTree(TreeNode* root) {
+        if(root == NULL){
+        return 0 ;
+    }        
+    queue< pair<TreeNode*,int> >  q ;
+    q.push({root,0}) ;
+    int ans = 0 ;
+    while(q.size()){
+        int size = q.size();
+        int minInd = 0 ;
+        int maxInd = 0 ;
+        int levelMin = q.front().second ;
+        for (int i = 0; i < size ; i++)
+        {
+            auto front = q.front() ;
+            q.pop() ;
+            TreeNode* node = front.first ;
+            int index = front.second - levelMin ;
+            if(i == 0 ){
+                minInd = index ;
+            }
+            if(i == size -1 ) {
+                maxInd = index ;
+            }
+            if(node -> left ){
+                q.push({node -> left ,2*index +1 }) ;
+            }
+            if(node -> right){
+                q.push({node -> right , 2*(index)+2 }) ;
+            }
+        }
+        ans = max(ans,maxInd - minInd + 1) ;
+    }
+    return ans ; 
+    }
+};
