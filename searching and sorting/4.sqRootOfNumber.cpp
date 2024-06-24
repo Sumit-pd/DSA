304a0497b47e00590464b9cab1920b02b2ef675a
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-06-24 19:00:39 +0530

    Update 4.sqRootOfNumber.cpp

--- searching and sorting/4.sqRootOfNumber.cpp
+++ searching and sorting/4.sqRootOfNumber.cpp
@@ -14,6 +14,7 @@ int squareRoot(int a)
 		return 0 ;
 	}
 	int ans = 1 ;
+	// we need to just find the smallest decimal value and for that we need to find the minimum square of a number
 	for(int i = 1 ; i <= a ; i++ ){
 		if(i*i <= a){
 			ans = i ;
@@ -35,7 +36,7 @@ int squareRoot(int a)
 	int low = 1 ;
 	int high = a ;
 	while(low <= high){
-		int mid = (low+high)/2 ;
+		long long mid = (low+high)/2 ; // when we multiply mid and mid the value may go out of range
 		if(mid*mid <= a){
 			low = mid+1;
 		}
