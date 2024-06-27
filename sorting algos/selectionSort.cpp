abe97aca1317167a4e9485180641cc55eb9f1437
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-06-27 21:53:28 +0530

    Update selectionSort.cpp

--- sorting algos/selectionSort.cpp
+++ sorting algos/selectionSort.cpp
@@ -2,11 +2,12 @@
 void selectionSort(vector<int>& arr, int n)
 {   
     for(int i = 0 ; i < n-1; i++){
-        //we will fix the minimum value of from i to n-1 to ith index
+        int mini = i ;
         for(int j = i + 1 ; j < n ; j++){
-            if(arr[j] < arr[i]){
-                swap(arr[i],arr[j]);
+            if(arr[mini] > arr[j] ){
+                mini = j ;
             }
         }
+        swap(arr[mini],arr[i]) ;
     }
 }
