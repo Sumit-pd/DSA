54b21d2c64421824b1c12827099d258fadbe8346
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-07-27 12:43:23 +0530

    add flatten Doubly ll

---
+++ linklists/flattenDoublelyLL.cpp
@@ -0,0 +1,37 @@
+#include<stack>
+using namespace std ;
+class Node {
+public:
+    int val;
+    Node* prev;
+    Node* next;
+    Node* child;
+};
+lass Solution {
+public:
+    Node* flatten(Node* head) {
+        stack<Node *> st ;
+        Node * temp = head ;
+        while(temp){
+            if(temp -> child){
+                // if there is a child that should coming to next of the temp
+                if(temp -> next){
+                    // if there is node next to it then the node should be kept in the stack as it will be added in the last 
+                    st.push(temp -> next) ;
+                    temp -> next -> prev = NULL ;
+                }
+                temp -> next = temp -> child ;
+                temp -> child -> prev = temp ;
+                temp -> child = NULL ;
+            }
+            if(!temp -> next && st.size()){
+                Node * top = st.top() ;
+                st.pop() ;
+                temp -> next = top ;
+                top -> prev = temp ;
+            }
+            temp = temp -> next  ;
+        }
+        return head ;
+    }
+};
