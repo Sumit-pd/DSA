e428ede7ecee85a1fbdb0bdb1b6c427442b04f09
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-07-28 14:25:50 +0530

    update quick sort and kthLargestElement

--- sorting algos/quickSort.cpp
+++ sorting algos/quickSort.cpp
@@ -1,45 +1,37 @@
-//pick a pivot arr[i]
-//place it at the correct position
-/*put all the smaller elements at the left part and the larger elements at the right */
-//now do the rest in recursion
-#include <bits/stdc++.h> 
+class Solution {
+    int partition(vector<int> &nums , int low , int high){
+        int randNum = rand()%(high-low + 1) + low;
+	    swap(nums[low],nums[randNum]) ;
+        int pivot = nums[low] ;
+        int i = low+1 ;
+        int j = high ;
+        while(i<=j){
+            while(i <= high && nums[i] <= pivot  ){
+                i++ ;
+            }
+            while(j >= low && nums[j] > pivot ){
+                j-- ;
+            }
+            if(i<j){
+                swap(nums[i],nums[j]) ;
+            }
+        }
 
-int findPartition(vector<int> &arr , int low , int high)
-{
-    int i = low ;
-    int j = high ;
-    int pivot = arr[low] ; // we will fix the first element as the pivot 
-    while(i < j ){
-        // we are keeping all the element smaller than pivot to its left and elements larger to its right
-        while(arr[i] <= pivot && i <= high ){
-            i++;
-        }
-        while(arr[j] > pivot && j>=0 ){
-            j--;
-        }
-        if(i<j){
-            swap(arr[i],arr[j]);
-        }
+        swap(nums[low],nums[j]) ;
+        return j ;
     }
-    //now place the pivot at its correct index
-    swap(arr[low],arr[j]) ;
-    return j ;
-}
-void helper(vector<int> &arr , int low , int high){
-    if(low >= high){
-        return ;
+    void helper(vector<int> &nums , int low , int high){
+        if(low < high) {
+            int part = partition(nums,low,high) ;
+            helper(nums,low,part-1) ;
+            helper(nums,part+1,high) ;
+        }
+        
     }
-    //find the pivot and put the pivot in the correct index and return its index as well
-    //the following index will also keep all the elements smaller than the pivot to its left a
-    //and all the element larger to its right
-    int partition = findPartition(arr,low,high);
-    helper(arr,low,partition-1);
-    helper(arr,partition+1,high);
+public:
+    vector<int> sortArray(vector<int>& nums) {
+        helper(nums,0,nums.size()-1) ;
+        return nums;
+    }
+};
 
-
-}
-vector<int> quickSort(vector<int> arr)
-{
-    helper(arr,0,arr.size()-1);
-    return arr ;
-}
