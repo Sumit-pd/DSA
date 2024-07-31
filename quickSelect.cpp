0cbe48566c2f8c7aacc46985849124d893ccb9a0
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-07-31 19:37:58 +0530

    add quick select and remove C_Cpp.errorSquiggles

--- quickSelect.cpp
+++ quickSelect.cpp
@@ -1,35 +1,36 @@
 class Solution {
     int partition(vector<int> &arr , int low , int high){
-	int randNum = rand()%(high-low + 1) + low;
-	swap(arr[low],arr[randNum]) ;
-	int pivot = arr[low] ;
-	int i = low +1;
-	int j = high ;
-	while(i <= j){
-		while(arr[i] < pivot && i <= j){
-			i++ ;
-		}
-		while(arr[j] >= pivot && i <= j){
-			j-- ;
-		}
-		if(i < j ){
-			swap(arr[i],arr[j]) ;
-		}
-	}
-	swap(arr[j],arr[low]) ;
-	return j;
-}
-int helper(vector<int> &arr ,int size, int K , int low , int high){
-	if(low > high) return -1 ;
+        int randNum = rand()%(high-low + 1) + low;
+        swap(arr[low],arr[randNum]) ;
+        int pivot = arr[low] ;
+        int i = low +1;
+        int j = high ;
+        while(i <= j){
+            while(arr[i] < pivot && i <= j){
+                i++ ;
+            }
+            while(arr[j] >= pivot && i <= j){
+                j-- ;
+            }
+            if(i < j ){
+                swap(arr[i],arr[j]) ;
+            }
+        }
+        swap(arr[j],arr[low]) ;
+        return j;
+        }
+        int helper(vector<int> &arr ,int size, int K , int low , int high){
+        if(low > high) return -1 ;
 
-	int part = partition(arr,low,high) ;
-	if(part == arr.size() - K){
-		return arr[part] ;
-	}
-	else if(part < arr.size() - K){
-		return helper(arr,size,K,part+1,high) ;
-	}
-	else{
-		return helper(arr,size,K,low,part-1) ;
-	}
+        int part = partition(arr,low,high) ;
+        if(part == arr.size() - K){
+            return arr[part] ;
+        }
+        else if(part < arr.size() - K){
+            return helper(arr,size,K,part+1,high) ;
+        }
+        else{
+            return helper(arr,size,K,low,part-1) ;
+        }
+    }
 }
