035abec098990b979ba82a1f0bf92bf79d898fe0
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-08-07 21:17:51 +0530

    add bitManipulation

---
+++ Bit Manipulation/bitManipulationOperation.cpp
@@ -0,0 +1,29 @@
+class BitManipulation{
+    public:
+    int getBit(int n , int i){
+        // the function to get bit at i'th position
+        int mask = 1 << i ;
+        if((n & mask) == 0){
+            return 0 ;
+        }
+        else{
+            return 1 ;
+        }
+    }
+
+    int setBit(int n , int i ){
+        // make the ith bit one
+        int mask = 1 << i ;
+        return n | mask ;
+    }
+
+    int toggle(int n , int i){
+        int mask = 1 << i ;
+        return n^i ;
+    }
+
+    int resetBit(int n , int i){
+        int mask = 1 << i ;
+        return ~n & mask ;
+    }
+};
