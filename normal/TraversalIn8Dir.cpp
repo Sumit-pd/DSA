b001a40b83488fb00be180bc3a5e765873559f0e
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-08-09 21:06:39 +0530

    utility file

---
+++ normal/TraversalIn8Dir.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std ;
+int main(){
+    int row ;
+    int col ;
+    for (int i = -1; i <= 1; i++){
+        for(int j = -1 ; j <= 1 ; j++ ){
+            int newRow = row + i ;
+            int newCol = col + j ;
+        }
+    }
+    
+}
