796ad085ec04e4ba813d31a6eb260c28ffe96202
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-08-18 15:34:38 +0530

    update check duplicates subtree

--- Binary Tree/checkDuplicatedSubtree.cpp
+++ Binary Tree/checkDuplicatedSubtree.cpp
@@ -20,12 +20,12 @@
 #include<bits/stdc++.h>
 int checkNode(BinaryTreeNode<char> * root ){
     if(root == NULL ){
-        return 0 ;
+        return 0 ; //this indicates the node does not exists
     }
     if(root -> left == NULL && root -> right == NULL ){
-        return 1 ;
+        return 1 ; // this indicates the node is a leaf node so it will not count in the answer as leaf node size is one
     }
-    return 2 ;
+    return 2 ; // now this is what we are looking for
 }
 void helper( BinaryTreeNode<char> *root , bool &ans , unordered_set<string> &st ){
     if(root == NULL ){
@@ -37,9 +37,11 @@ void helper( BinaryTreeNode<char> *root , bool &ans , unordered_set<string> &st 
     int right = checkNode(root -> right) ;
 
     if(left == 0 && right == 0 ){
+        // it is leaf node 
         return ;
     }
     else if(left == 1 && right == 0){
+        // the node has one child at the left
         string s = "" ;
         s += root -> data ;
         s += root -> left -> data ;
@@ -51,6 +53,7 @@ void helper( BinaryTreeNode<char> *root , bool &ans , unordered_set<string> &st 
         st.insert(s) ;
     }
     else if(left == 0 && right == 1 ){
+        // the node has one child at the right
         string s = "" ;
         s += root -> data ;
         s += "$" ;
@@ -62,6 +65,7 @@ void helper( BinaryTreeNode<char> *root , bool &ans , unordered_set<string> &st 
         st.insert(s) ;
     }
     else if (left == 1 && right == 1 ){
+        // the node has two child 
         string s = "" ;
         s += root -> data ;
         s += root -> left -> data ;
@@ -74,9 +78,11 @@ void helper( BinaryTreeNode<char> *root , bool &ans , unordered_set<string> &st 
     }
     else {
         if(left == 2){
+            // the node has more than one child at the left
             helper(root -> left , ans , st ) ;
         }
         if(right == 2 ){
+            // the node has more than one child at the right
             helper(root -> right , ans , st ) ;
         }
     }
@@ -87,4 +93,79 @@ bool similarSubtree(BinaryTreeNode<char> *root)
     bool ans = false; 
     helper(root,ans,st) ;
     return ans ;
+}
+
+
+
+
+
+#include<bits/stdc++.h>
+int checkNodeType(BinaryTreeNode<char> * root){
+    if(root == NULL){
+        return 0 ;
+    }
+    if(root -> left == NULL && root -> right == NULL){
+        return 1 ;
+    }
+
+    return 2 ;
+}
+bool helper(BinaryTreeNode<char> * root ,unordered_set<string> &st){
+    if(root == NULL){
+        return false ;
+    }
+
+    int left = checkNodeType(root -> left) ;
+    int right = checkNodeType(root ->right ) ;
+    if(left == 0 && right == 0){
+        return false ;
+    }
+    else if(left == 1 && right == 0){
+        string smallString = "" ;
+        smallString += root -> data ;
+        smallString += root -> left -> data ;
+        smallString += "$";
+        if(st.find(smallString) != st.end()){
+            return true ;
+        }
+        st.insert(smallString) ;
+    }
+    else if(left == 0 && right == 1){
+        string smallString = "" ;
+        smallString += root -> data ;
+        smallString += "$" ;
+        smallString += root -> right -> data ;
+        if(st.find(smallString) != st.end()){
+            return true ;
+        }
+        st.insert(smallString) ;
+    }
+    else if(left == 1&& right == 1){
+        string smallString = "" ;
+        smallString += root -> data ;
+        smallString += root -> left ->data ;
+        smallString += root -> right -> data ;
+        if(st.find(smallString) != st.end()){
+            return true ;
+        }
+        st.insert(smallString) ;
+    }
+    else {
+        if(left == 2){
+            if(helper(root->left,st) == true){
+                return true ;
+            }
+        }
+        if(right == 2){
+            if(helper(root->right,st) == true){
+                return true ;
+            }
+        }
+    }
+    return false ;
+}
+bool similarSubtree(BinaryTreeNode<char> *root)
+{	
+    unordered_set<string> st ;
+    return helper(root,st) ;
 }
