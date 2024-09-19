c45f73ef9c286dbef0d4c321e7a9a16b8cb775b9
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-09-19 14:53:29 +0530

    fix some issues and add code

---
+++ Binary Tree/morrisOrderTraversal.cpp
@@ -0,0 +1,66 @@
+//for inorder
+vector<int> morrisTraversal(Node * root){
+    if(root == NULL){
+        return root ;
+    }
+    vector<int> ans ;
+    Node * cur = root;
+    while(cur != NULL ){
+        if(cur -> left == NULL){
+            ans.push_back(cur -> data) ;
+            cur = cur -> next ;
+        }
+        else{
+            Node * temp = root -> left ;
+            while(temp -> right != NULL && temp != cur){
+                temp = temp -> right ;
+            }
+            if(temp -> right == NULL){
+                temp -> right = cur ;
+                cur = cur -> left ;
+            }
+            else if(temp -> right = cur){
+                temp -> right = NULL ;
+                ans.push_back(cur -> data) ;
+                cur = cur -> right ;
+            }
+        }
+    }
+    return ans;
+}
+
+
+
+//for postorder
+vector<int> morrisTraversal(Node * root){
+    if(root == NULL){
+        return root ;
+    }
+    vector<int> ans ;
+    Node * cur = root;
+    while(cur != NULL ){
+        if(cur -> left == NULL){
+            ans.push_back(cur -> data) ;
+            cur = cur -> next ;
+        }
+        else{
+            Node * temp = root -> left ;
+            while(temp -> right != NULL && temp != cur){
+                temp = temp -> right ;
+            }
+            if(temp -> right == NULL){
+                temp -> right = cur ;
+                ans.push_back(cur -> data) ;
+                cur = cur -> left ;
+            }
+            else if(temp -> right = cur){
+                temp -> right = NULL ;
+                cur = cur -> right ;
+            }
+        }
+    }
+    return ans;
+}
+
+// this uses the concept of threaded binary tree
+// time complexity : O(N) + O(N)
