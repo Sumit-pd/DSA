3d313bd16c22602d7f03f2c3472fa0c6ceac1c21
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-10-12 20:08:07 +0530

    add dp solution to wordbreak problem

--- strings/wordBreak.cpp
+++ strings/wordBreak.cpp
@@ -34,3 +34,51 @@ public:
         return helper(0,st,n,A,dp);
     }
 };
+
+
+
+class Solution
+{
+public:
+    int helper(int ind , unordered_set<string> st, int n , string target ,vector<int> &dp ){
+        if(ind == n){
+            return 1 ;
+        }
+        if(dp[ind] != -1){
+            return dp[ind];
+        }
+        string temp = "";
+        for(int i = ind ; i < n ; i++ ){
+            temp += target[i];
+            if(st.find(temp) != st.end()){
+                if(helper(i+1, st,n,target,dp)){
+                    return dp[ind]=1;
+                }
+            }
+        }
+        return dp[ind]=0;
+        
+    }
+    bool wordBreak(string target, vector<string>& B) {
+       vector<bool> dp(target.size()+1,false);
+        unordered_set<string> st ;
+        for(auto it:B){
+            st.insert(it);
+        }
+        int n = target.size();
+        dp[n] = true ;
+        for(int ind = n-1 ; ind >= 0 ; ind-- ){
+            string temp = "";
+            for(int i = ind ; i < n ; i++ ){
+                temp += target[i];
+                if(st.find(temp) != st.end()){
+                    if(dp[i+1]){
+                        dp[ind]=1;
+                        break ;
+                    }
+                }
+            }
+        }
+        return dp[0] ;
+    }
+};
