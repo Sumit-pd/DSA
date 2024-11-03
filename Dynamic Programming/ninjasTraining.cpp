a0ac5e48e1cd36a122a931a054b9916b472ff379
Sumit-pd <88550925+Sumit-pd@users.noreply.github.com>
2024-11-03 11:41:49 +0530

    ninjas training update

--- Dynamic Programming/ninjasTraining.cpp
+++ Dynamic Programming/ninjasTraining.cpp
@@ -1,34 +1,53 @@
 //memoization
-int helper(int ind , int prev , vector<vector<int>> &points , int n ,
-vector<vector<int>> &dp ){
-
-    if(ind == 0 ){
+#include<bits/stdc++.h>
+int helper(int day , int last , vector<vector<int>> &points, vector<vector<int>> &dp ){
+    if(day == 0){
         int maxi = 0 ;
         for(int i = 0 ; i < 3 ; i++ ){
-            if(i != prev  ){
-                maxi = max(maxi , points[0][i] ) ;
+            if(i!=last){
+                maxi = max(maxi,points[0][i]) ;
             }
         }
-        return maxi;
+        return maxi ;
     }
-    if(dp[ind][prev] != -1 ){
-        return dp[ind][prev] ;
+    if(dp[day][last] != -1){
+        return dp[day][last] ;
     }
-
     int maxi = 0 ;
     for(int i = 0 ; i < 3 ; i++ ){
-        if(i != prev ){
-            int point = points[ind][i] + helper(ind-1,i,points,n,dp) ;
+        if(i != last){
+            int point = points[day][i] + helper(day-1,i,points,dp) ;
             maxi = max(maxi,point) ;
         }
     }
-
-    return dp[ind][prev] = maxi ;
+    return dp[day][last] = maxi ;
 }
 int ninjaTraining(int n, vector<vector<int>> &points)
 {
     vector<vector<int>> dp(n,vector<int>(4,-1)) ;
-    return helper(n-1,3,points,n,dp) ;
+    return helper(n-1,3,points,dp) ;
 }
 
 /********************/
+//Tabulation
+int ninjaTraining(int n, vector<vector<int>> &points)
+{
+    vector<vector<int>> dp(n,vector<int>(4,0)) ;
+    dp[0][0] = max(points[0][1], points[0][2]);
+    dp[0][1] = max(points[0][0], points[0][2]);
+    dp[0][2] = max(points[0][0], points[0][1]);
+    dp[0][3] = max({points[0][0], points[0][1], points[0][2]});
+    for(int day = 1 ; day < n ; day++ ){
+        for(int last = 0 ; last < 4 ; last++ ){
+            int maxi = 0 ;
+            for(int i = 0 ; i < 3 ; i++ ){
+                if(i != last) {
+                    int point = points[day][i] + dp[day - 1][i];
+                    maxi = max(maxi, point);
+                }
+            }
+            dp[day][last] = maxi ;
+        }
+    }
+    return dp[n-1][3] ;
+}
